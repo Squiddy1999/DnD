d5b7413794e70b930a1215af3b3e302b7e4dba8b
Alex Brown <16653219@students.lincoln.ac.uk>
2019-03-03 19:10:16 +0000

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
     console.log("Ready");
+    button();
 });
+
+function button() {
+    $("#indexLoginButton").click(function () {
+        console.log("Click!");
+        window.location = "userHome.html";
+        console.log(window.location);
+    });
+}
